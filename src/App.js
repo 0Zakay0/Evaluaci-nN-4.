2b3f36f13c9292083da19162ddd58b9f39d356e6
Tecnologías de la Información <tic.calama@inacap.cl>
2023-07-07 14:44:08 -0400

    primer proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from "react";
+import { Route, Routes, Link } from "react-router-dom"
+
+const App = () => {
+  return (
+    <>
+    <h1>Nueva pagina de inicio</h1>
+    <p>asdasdasdasdasdads</p>
+    </>
+  )
+}
+
+export default App;
